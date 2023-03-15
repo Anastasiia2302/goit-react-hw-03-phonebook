f7838685fba162b4688284c8533fa5a5c2f1cbaf
Anastasiia Herusova <nastia.herusova@gmail.com>
2023-03-15 11:21:02 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,10 @@ import { Container, Title } from 'PhoneBook.styled';
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
 
     filter: '',
@@ -19,17 +19,13 @@ class App extends Component {
   handlerSubmit = data => {
     const isExist = this.state.contacts.find(
       contact => contact.name.toLowerCase() === data.name.toLowerCase()
-    )
+    );
     if (isExist) {
-      alert(`${data.name} is already in contacts`)
-    return;}
-    
+      alert(`${data.name} is already in contacts`);
+      return;
+    }
 
-
-   this.setState(prevState => ({contacts:[...prevState.contacts, data] }));
-   
-    
-    
+    this.setState(prevState => ({ contacts: [...prevState.contacts, data] }));
   };
 
   onFilter = element => {
@@ -42,6 +38,18 @@ class App extends Component {
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContact = JSON.parse(contacts);
+    this.setState({ contacts: parsedContact });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const { contacts, filter } = this.state;
