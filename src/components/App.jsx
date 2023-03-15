68ab31cae49e88df4329b99aef24fafc04ddc68c
Anastasiia Herusova <nastia.herusova@gmail.com>
2023-03-15 13:32:12 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -42,10 +42,13 @@ class App extends Component {
   componentDidMount() {
     const contacts = localStorage.getItem('contacts');
     const parsedContact = JSON.parse(contacts);
-    this.setState({ contacts: parsedContact });
+    if(parsedContact) {
+      this.setState({ contacts: parsedContact });
+    }
+   
   }
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(prevState) {
     if (this.state.contacts !== prevState.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
